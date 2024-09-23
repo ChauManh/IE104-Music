5086f3147f4380a17cb0ee38c1fa7ff646d3b477
Duong Anh Vu <22521688@gm.uit.edu.vn>
2024-09-23 21:28:10 +0700

    update

--- Frontend/script.js
+++ Frontend/script.js
@@ -8,3 +8,16 @@ showFriendsButton.addEventListener('focus', () => {
 showFriendsButton.addEventListener('blur', () => {
   friendList.classList.add('hidden');
 });
+
+
+document.getElementById('playlist-items').addEventListener('click', function() {
+    // Lấy danh sách các thẻ li bổ sung
+    const extraList = document.getElementById('extra-list');
+    
+    // Toggle class 'hidden' để hiện/ẩn danh sách
+    if (extraList.classList.contains('hidden')) {
+      extraList.classList.remove('hidden'); // Hiển thị các thẻ li
+    } else {
+      extraList.classList.add('hidden'); // Ẩn các thẻ li
+    }
+});
