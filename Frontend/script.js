6713db18c2824497bf764f5b33bb1f7fce34ca65
Duong Anh Vu <22521688@gm.uit.edu.vn>
2024-09-23 21:59:31 +0700

    update

--- Frontend/script.js
+++ Frontend/script.js
@@ -11,13 +11,35 @@ showFriendsButton.addEventListener('blur', () => {
 
 
 document.getElementById('playlist-items').addEventListener('click', function() {
-    // Lấy danh sách các thẻ li bổ sung
     const extraList = document.getElementById('extra-list');
-    
-    // Toggle class 'hidden' để hiện/ẩn danh sách
     if (extraList.classList.contains('hidden')) {
-      extraList.classList.remove('hidden'); // Hiển thị các thẻ li
+      extraList.classList.remove('hidden');
     } else {
-      extraList.classList.add('hidden'); // Ẩn các thẻ li
+      extraList.classList.add('hidden');
+    }
+});
+
+const playButton = document.getElementById('playBtn');
+
+playButton.addEventListener('click', function() {
+    if (playButton.src.includes('play.png')) {
+        playButton.src = '/Frontend/src/assets/pause.png'; // Hình ảnh khi đang phát
+    } else {
+        playButton.src = '/Frontend/src/assets/play.png'; // Hình ảnh khi dừng phát
+    }
+});
+
+
+const volumeIcon = document.getElementById('volumeIcon');
+const volumeControl = document.getElementById('volumeControl');
+
+let previousVolume = volumeControl.value;
+
+volumeIcon.addEventListener('click', function() {
+    if (volumeControl.value === '0') {
+        volumeControl.value = previousVolume; 
+    } else {
+        previousVolume = volumeControl.value;
+        volumeControl.value = 0; 
     }
 });
