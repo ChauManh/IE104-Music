b2bcec5d29cd53037b5605345bdd4e8a6a107ac1
ChauManh <chaumanh1108@gmail.com>
2024-11-05 14:01:41 +0700

    update router playlist, track

--- Backend/config/db/db.js
+++ Backend/config/db/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Soundtify');
+    await mongoose.connect(process.env.MONGODB_URL);
     console.log('Connect succesfully!!!');
   } catch (error) {
     console.log('Connect fail!!!');
