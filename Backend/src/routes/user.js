4a1f7bfe71ba38e9d691ad1c2573c298c1e9953d
ChauManh <chaumanh1108@gmail.com>
2024-12-03 14:13:21 +0700

    fix api createplaylist

--- Backend/src/routes/user.js
+++ Backend/src/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../middleware/auth');
 const UserController = require('../controllers/UserController');
 
-router.get('/create_playlist', UserController.createPlaylist);
+router.post('/create_playlist', UserController.createPlaylist);
 router.get('/get_playlists', UserController.getUserPlaylists);
 router.post('/playlist/add_song', UserController.addSongToPlaylist);
 router.delete('/playlist/remove_song', UserController.removeSongFromPlaylist);
