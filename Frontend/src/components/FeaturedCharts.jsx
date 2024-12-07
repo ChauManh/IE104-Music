0597c3fdb676212d3b15b5b5e0c5459e00e4097a
trungtran0165 <127908904+trungtran0165@users.noreply.github.com>
2024-12-07 10:23:24 +0700

    Fetch and display user playlists in sidebar; update FeaturedCharts to allow full album display

--- Frontend/src/components/FeaturedCharts.jsx
+++ Frontend/src/components/FeaturedCharts.jsx
@@ -31,9 +31,9 @@ const FeaturedCharts = () => {
  return (
   <div className='mb-6'>
     <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
-    <div className='flex overflow-auto'>
+    <div className='flex overflow-x-scroll'>
       {albumsData.length > 0 ? (
-        albumsData.slice(0,4).map((item, index) => ( // Chỉ lấy 5 phần tử đầu tiên
+        albumsData.map((item, index) => ( // Chỉ lấy 5 phần tử đầu tiên
           <AlbumItem 
             key={index} 
             name={item.name} 
