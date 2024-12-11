91ae7ea3a6a1a02250205c4ba502d8d7004478c8
ChauManh <chaumanh1108@gmail.com>
2024-12-11 22:59:35 +0700

    fix conflict merge

--- Backend/src/routes/album.js
+++ Backend/src/routes/album.js
@@ -2,6 +2,7 @@ const express = require('express');
 const AlbumController = require('../controllers/AlbumController');
 const router = express.Router();
 
+router.get('/new', AlbumController.getNewReleases);
 router.get('/:id', AlbumController.getAlbum);
 router.get('/:id/tracks', AlbumController.getAlbumTracks);
 
